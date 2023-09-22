1ed410a0dadc2bc634ef474cccad9a84e62b44a6
LavanyaThangapandiyan <128019444+LavanyaThangapandiyan@users.noreply.github.com>
2023-09-22 17:39:05 +0530

    Update EmployeeDAO.java

--- EmployeeDAO.java
+++ EmployeeDAO.java
@@ -2,13 +2,25 @@ package com.chainsys.core.dao;
 
 import java.util.List;
 import java.util.Map;
-
 import com.chainsys.appplatform.exception.AppException;
 import com.chainsys.base.db.JDBCTransaction;
 import com.chainsys.core.util.AppConnectWrapper;
 import com.chainsys.core.vo.EmployeeDefinitionVO;
+import com.chainsys.core.vo.UserProfileVO;
 
 public class EmployeeDefinitionDAO {
+	
+	public void delete(JDBCTransaction jdbcTransaction, int employeeId) throws AppException {
+	    StringBuilder deleteQuery = null;
+	    try {
+	        deleteQuery = new StringBuilder();
+	        deleteQuery.append(" DELETE FROM EMPLOYEE_3458 WHERE EMPLOYEE_ID = ? ");
+	        jdbcTransaction.getSpringUtils().update(deleteQuery.toString(), new Object[] {employeeId});
+	    } catch (Exception exception) {
+	        throw new AppException(getClass().getName(), "delete", exception.getMessage(), exception);
+	    }
+	}
+	
 	public EmployeeDefinitionVO save(JDBCTransaction jdbcTransaction, EmployeeDefinitionVO employeeDefinitionVO)
 			throws AppException {
 		try {
@@ -22,11 +34,12 @@ public class EmployeeDefinitionDAO {
 		}
 		return employeeDefinitionVO;
 	}
-	public List<Map<String, Object>> fetchAll(int loggedUserId, int organizationId) throws AppException {
+	public List<Map<String, Object>> fetchAll(UserProfileVO userProfileVO) throws AppException {
 		StringBuilder fetchEmployeeNameQuery = null;
 		List<Map<String, Object>> employeeDefinitionWithPrivilegeList = null;
 		JDBCTransaction jdbcTransaction = null;
 		try {
+			int organizationId =userProfileVO.getOrgId();
 			jdbcTransaction = new JDBCTransaction(organizationId, false);
 			fetchEmployeeNameQuery = new StringBuilder();
 			fetchEmployeeNameQuery.append(" SELECT EMPLOYEE_NAME  AS \"EMPLOYEE_NAME\", ");
